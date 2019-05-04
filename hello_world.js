c6e8317705f3920d8f28265673733d61845a7b22
mkhuthir <mkhuthir@gmail.com>
2019-05-04 12:15:16 +0400

    added display image

--- hello_world.js
+++ hello_world.js
@@ -6,3 +6,5 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello Node.js World!');
 }).listen(8080);
+
+console.log("Server running at http://localhost:8080/");
--- myfirstmodule_use.js
+++ myfirstmodule_use.js
@@ -8,3 +8,5 @@ http.createServer(function (req, res) {
   res.write("The date and time are currently: " + dt.myDateTime());
   res.end();
 }).listen(8080);
+
+console.log("Server running at http://localhost:8080/");
